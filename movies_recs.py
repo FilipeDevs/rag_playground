0efb434b885b43ad8db2f818182379d762728a53
Filipe Pereira <filipe.pereira.dev@gmail.com>
2024-02-17 21:38:24 +0100

    some vector searching in movies db

--- movies_recs.py
+++ movies_recs.py
@@ -1,7 +1,8 @@
+from generate_embedding import generate_embedding
 import pymongo
 import os
 from dotenv import load_dotenv
-import requests
+
 
 
 load_dotenv()
@@ -15,19 +16,25 @@ collection = db.movies
 hugging_face_api_key = os.getenv("HUGGING_FACE_API_KEY")
 embedding_url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
 
-def generate_embedding(text : str) -> list[float]:
 
-    response = requests.post(
-        embedding_url,
-        headers={"Authorization": f"Bearer {hugging_face_api_key}"},
-        json={"inputs": text})
-    
-    if response.status_code != 200:
-        raise ValueError(f"Request failed with status code {response.status_code}")
-    
-    return response.json()
+query = "Imaginary characters from outer space at war"
 
 
-print(generate_embedding("freeCodeCamp is awesome"))
+# Use the $vectorSearch aggregation stage to find the most similar documents to the query
+# MongoDB will use the plot_embedding_hf field to find the most similar documents based on the embedding of the query
+# The numCandidates parameter specifies the number of documents to consider for the search
+# The limit parameter specifies the number of documents to return
+# The index parameter specifies the name of the index to use for the search (created in MongoDB Atlas Search)
+results = collection.aggregate([
+    {"$vectorSearch": {
+        "queryVector" : generate_embedding(query),
+        "path": "plot_embedding_hf",
+        "numCandidates" : 100,
+        "limit" : 4,
+        "index" : "plot_semantic_search",
+    }}
+])
 
+for document in results:
+    print(f'Movie Name: {document["title"]}, \nMovie Plot: {document["plot"]}\n')
 
